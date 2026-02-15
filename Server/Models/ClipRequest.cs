e1eb260220a937a9d49f7fbe6590ea79a8df91d2
jeantou666 <olivier.hersin@gmail.com>
2026-02-15 18:31:53 +0100

    Create ClipRequest.cs

---
+++ Server/Models/ClipRequest.cs
@@ -0,0 +1,10 @@
+namespace ClipShare.Models
+{
+    public class ClipRequest
+    {
+        public string ItemId { get; set; } = "";
+        public double StartSeconds { get; set; }
+        public double EndSeconds { get; set; }
+        public int ExpireHours { get; set; }
+    }
+}
