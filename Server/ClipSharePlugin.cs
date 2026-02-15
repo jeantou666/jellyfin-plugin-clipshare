04cf76276f4a71cbb24a91fd4ecb027aefa601cc
jeantou666 <olivier.hersin@gmail.com>
2026-02-15 20:59:12 +0100

    Update ClipSharePlugin.cs

--- Server/ClipSharePlugin.cs
+++ Server/ClipSharePlugin.cs
@@ -7,37 +7,19 @@ using System.Collections.Generic;
 using System.Linq;
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Model.Plugins;
-
+using MediaBrowser.Model.Serialization;
 
 
 namespace ClipShare
 {
-    public class ClipSharePlugin : BasePlugin<PluginConfiguration>, IHasWebPages
+    public ClipSharePlugin(
+    IServerApplicationPaths applicationPaths,
+    IXmlSerializer xmlSerializer)
+    : base(applicationPaths, xmlSerializer)
     {
-        public override string Name => "ClipShare";
-        public override Guid Id => Guid.Parse("7f4a3b2c-6d5e-4a11-9c2b-5e3a7d4f8a21");
-
-        public ClipSharePlugin(IServerApplicationPaths applicationPaths, ILogger<ClipSharePlugin> logger)
-            : base(applicationPaths, logger)
-        {
-            Instance = this;
-        }
-
-        public static ClipSharePlugin? Instance { get; private set; }
-
-        public override string Description =>
-            "Create temporary public shareable clips from videos.";
-
-        public IEnumerable<PluginPageInfo> GetPages() => new[]
-        {
-            new PluginPageInfo
-            {
-                Name = "clipshare",
-                EmbeddedResourcePath = GetType().Namespace + ".Web.index.html"
-            }
-        };
-
+        Instance = this;
     }
+
 
     public class PluginConfiguration : BasePluginConfiguration
     {
