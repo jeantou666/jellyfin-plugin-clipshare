7569770b07846def54e89c0962399ba748887549
jeantou666 <olivier.hersin@gmail.com>
2026-02-15 21:01:53 +0100

    Update ClipSharePlugin.cs

--- Server/ClipSharePlugin.cs
+++ Server/ClipSharePlugin.cs
@@ -1,29 +1,34 @@
 using System;
-using MediaBrowser.Common.Plugins;
-using MediaBrowser.Controller;
-using MediaBrowser.Model.Plugins;
-using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
-using System.Linq;
 using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
 
+namespace ClipShare;
 
-namespace ClipShare
+public class ClipSharePlugin : BasePlugin<PluginConfiguration>, IHasWebPages
 {
+    public static ClipSharePlugin? Instance { get; private set; }
+
+    public override string Name => "ClipShare";
+
+    public override Guid Id => Guid.Parse("7f4a3b2c-6d5e-4a11-9c2b-5e3a7d4f8a21");
+
     public ClipSharePlugin(
-    IServerApplicationPaths applicationPaths,
-    IXmlSerializer xmlSerializer)
-    : base(applicationPaths, xmlSerializer)
+        IServerApplicationPaths applicationPaths,
+        IXmlSerializer xmlSerializer)
+        : base(applicationPaths, xmlSerializer)
     {
         Instance = this;
     }
 
-
-    public class PluginConfiguration : BasePluginConfiguration
+    public IEnumerable<PluginPageInfo> GetPages() => new[]
     {
-        public int DefaultExpirationHours { get; set; } = 72;
-        public string ClipFolder { get; set; } = "clipshare";
-    }
+        new PluginPageInfo
+        {
+            Name = "clipshare",
+            EmbeddedResourcePath = $"{GetType().Namespace}.Web.index.html"
+        }
+    };
 }
