61f257b26ff6b39feb5c5e3e64ed77186215fb23
jeantou666 <jeantou666@gmail.com>
2026-02-20 20:12:22 +0000

    Fix: Add IApplicationPaths parameter to plugin constructor

--- Server/ClipSharePlugin.cs
+++ Server/ClipSharePlugin.cs
@@ -1,6 +1,7 @@
 using System;
 using ClipShare.Configuration;
 using MediaBrowser.Common.Plugins;
+using MediaBrowser.Controller;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
 
@@ -19,8 +20,8 @@ public class ClipSharePlugin : BasePlugin<PluginConfiguration>
 
     public override Guid Id => Guid.Parse("7f4a3b2c-6d5e-4a11-9c2b-5e3a7d4f8a21");
 
-    public ClipSharePlugin(IXmlSerializer xmlSerializer)
-        : base(xmlSerializer)
+    public ClipSharePlugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+        : base(applicationPaths, xmlSerializer)
     {
         Instance = this;
     }
