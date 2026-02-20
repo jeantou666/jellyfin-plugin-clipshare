6e12dc005e8b4d432595f1a0bb885df853bad541
jeantou666 <jeantou666@gmail.com>
2026-02-20 20:24:59 +0000

    Fix: Restore working plugin constructor with IServerApplicationPaths from v1.5.14

--- Server/ClipSharePlugin.cs
+++ Server/ClipSharePlugin.cs
@@ -1,7 +1,8 @@
 using System;
+using System.Collections.Generic;
 using ClipShare.Configuration;
-using MediaBrowser.Common;
 using MediaBrowser.Common.Plugins;
+using MediaBrowser.Controller;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
 
@@ -10,7 +11,7 @@ namespace ClipShare;
 /// <summary>
 /// ClipShare Plugin - Allows creating video clips from the Jellyfin player.
 /// </summary>
-public class ClipSharePlugin : BasePlugin<PluginConfiguration>
+public class ClipSharePlugin : BasePlugin<PluginConfiguration>, IHasWebPages
 {
     public static ClipSharePlugin? Instance { get; private set; }
 
@@ -20,9 +21,20 @@ public class ClipSharePlugin : BasePlugin<PluginConfiguration>
 
     public override Guid Id => Guid.Parse("7f4a3b2c-6d5e-4a11-9c2b-5e3a7d4f8a21");
 
-    public ClipSharePlugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+    public ClipSharePlugin(
+        IServerApplicationPaths applicationPaths,
+        IXmlSerializer xmlSerializer)
         : base(applicationPaths, xmlSerializer)
     {
         Instance = this;
     }
+
+    public IEnumerable<PluginPageInfo> GetPages() => new[]
+    {
+        new PluginPageInfo
+        {
+            Name = "clipshare",
+            EmbeddedResourcePath = $"{GetType().Namespace}.Web.index.html"
+        }
+    };
 }
